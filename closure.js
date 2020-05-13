aa2cdcf97270e0a081c1851222e3eb4241cf26c9
johnnyarza <johnnyarza@gmail.com>
2020-05-13 13:23:31 -0300

    closure

---
+++ closure.js
@@ -0,0 +1,18 @@
+//Closure é o escopo criado quando uma função é decalrada 
+//Esse escopo permite a função acessar e manipular variaveis exxternas à funçõa
+
+//Contexto léxico em ação
+
+const x = 'Global'
+
+function fora () {
+    const x = 'Local'
+    function dentro () {
+        return x
+    }
+
+    return dentro
+}
+
+const minhaFuncao = fora ()
+console.log(minhaFuncao())
